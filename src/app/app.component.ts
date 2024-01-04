5f871aaece87135c6cb2be062d6816cfc5ee5317
Braden <braden.whaley7@gmail.com>
2024-01-04 14:57:09 -0500

    1st Revision

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,4 +11,44 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'my-portfolio';
+  dynamicText = '';
+  textArray = ['C++', 'C#', 'Apps', 'Webpages', 'Automation Software', 'Software Plugins'];
+  currentIndex = -1;
+
+  ngOnInit() {
+    this.typeWriterEffect();
+  }
+
+  typeWriterEffect() {
+    this.currentIndex++;
+    if (this.currentIndex === this.textArray.length) {
+      this.currentIndex = 0;
+    }
+    const text = this.textArray[this.currentIndex];
+    let i = 0;
+    const typingInterval = setInterval(() => {
+      if (i < text.length) {
+        this.dynamicText += text[i];
+        i++;
+      } else {
+        clearInterval(typingInterval);
+        setTimeout(() => {
+          this.deleteText();
+        }, 2000);
+      }
+    }, 200);
+  }
+
+  deleteText(){
+    let i = this.dynamicText.length;
+    const deletingInterval = setInterval(() => {
+      if (i > 0) {
+        this.dynamicText = this.dynamicText.slice(0, -1);
+        i--;
+      } else {
+        clearInterval(deletingInterval);
+        this.typeWriterEffect();
+      }
+    }, 200);
+  }
 }
